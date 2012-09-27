8a0eab44d1146ea34a74fa4b8446b21b0c7c6330
behrooz shabani (everplays) <behrooz@rock.com>
2012-09-27 22:33:26 +0330

    use colon for resource spliter instead of period
    as hasCredential method of AgaviZendaclSecurityUser explodes credential
    with period we needed to change the splitter to something else to avoid
    conflict between resource and action that needs to be checked.

--- app/lib/action/MarketBaseAction.class.php
+++ app/lib/action/MarketBaseAction.class.php
@@ -9,22 +9,18 @@ class MarketBaseAction extends AgaviAction
     /**
 	 * Checks permissions to perform the operation. Called after validation.
 	 *
-	 *
-	 * @param      AgaviUser              The AgaviUser object (for convenience).
-	 * @param      AgaviRequestDataHolder The request data (for convenience).
-	 *
 	 * @return     bool Whether or not the operation is allowed.
 	 *
 	 * @author     fzerorubigd <fzerorubigd@gmail.com>
 	 * @since      1.1.0
 	 */
-	public function checkPermissions(AgaviUser $user, AgaviRequestDataHolder $rd)
+	public function getCredentials()
 	{
-        $actionName = trim(get_class($this), "\\");
-        $actionName = strtolower(str_replace('_', '.', substr($actionName, 0, -6)));
+        $actionName = substr(get_class($this), 0, -6);
+        $actionName = strtolower(str_replace('_', ':', $actionName));
         
         $method = $this->getContext()->getRequest()->getMethod();
         
-        return $user->isAllowed($actionName, $method);
+        return "$actionName.$method";
 	}
 }
