f738eff5aec278d84ea1a604111932c88f6cc19d
Fzerorubigd <fzerorubigd@gmail.com>
2012-09-27 21:14:28 +0330

    change the security check

--- app/lib/action/MarketBaseAction.class.php
+++ app/lib/action/MarketBaseAction.class.php
@@ -5,7 +5,34 @@
  */
 class MarketBaseAction extends AgaviAction
 {
+    
+    /**
+	 * Checks permissions to perform the operation. Called after validation.
+	 *
+	 *
+	 * @param      AgaviUser              The AgaviUser object (for convenience).
+	 * @param      AgaviRequestDataHolder The request data (for convenience).
+	 *
+	 * @return     bool Whether or not the operation is allowed.
+	 *
+	 * @author     fzerorubigd <fzerorubigd@gmail.com>
+	 * @since      1.1.0
+	 */
+	public function checkPermissions(AgaviUser $user, AgaviRequestDataHolder $rd)
+	{
+		// get the cool stuff
+		$context    = $this->getContext();
+		$user       = $context->getUser();
+        $container  = $this->getContainer();
 
+		// get the current action instance
+		$actionInstance = $container->getActionInstance();
+        
+        $actionName = trim(get_class($actionInstance), "\\");
+        $actionName = strtolower(str_replace('_', '.', substr($actionName, 0, -6)));
+        
+        $method = $this->getContext()->getRequest()->getMethod();
+        
+        return $user->isAllowed($actionName, $method);
+	}
 }
-
-?>
