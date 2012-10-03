bad7a8dd3bf31e53f7b46ce1ca43bb7438a481ae
behrooz shabani (everplays) <behrooz@rock.com>
2012-10-03 13:10:31 +0330

    replaced tabs with spaces for indention

--- app/modules/Appliance/views/IndexSuccessView.class.php
+++ app/modules/Appliance/views/IndexSuccessView.class.php
@@ -30,25 +30,25 @@
  */
 class Appliance_IndexSuccessView extends MarketApplianceBaseView
 {
-	
+    
 
-	/**
-	 * Handles the Html output type.
-	 *
-	 * @param AgaviRequestDataHolder $rd the (validated) request data
-	 *
-	 * @return     mixed <ul>
-	 *                     <li>An AgaviExecutionContainer to forward the execution to or</li>
-	 *                     <li>Any other type will be set as the response content.</li>
-	 *                   </ul>
-	 */
-	public function executeHtml(AgaviRequestDataHolder $rd)
-	{
-		$this->setupHtml($rd);
+    /**
+     * Handles the Html output type.
+     *
+     * @param AgaviRequestDataHolder $rd the (validated) request data
+     *
+     * @return     mixed <ul>
+     *                     <li>An AgaviExecutionContainer to forward the execution to or</li>
+     *                     <li>Any other type will be set as the response content.</li>
+     *                   </ul>
+     */
+    public function executeHtml(AgaviRequestDataHolder $rd)
+    {
+        $this->setupHtml($rd);
 
-		$this->setAttribute('_title', 'Index');
+        $this->setAttribute('_title', 'Index');
         $this->getLayer('content')->setSlot(
-            'tags', 
+            'tags',
             $this->createSlotContainer(
                 'Appliance', // name of module to use
                 'Tags', // name of action to execute
@@ -57,5 +57,5 @@ class Appliance_IndexSuccessView extends MarketApplianceBaseView
                 'read' // request method to use
             )
         );
-	}
+    }
 }
