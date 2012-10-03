27c182383d69ed96f1a65c61685bccffbcb3d4c1
behrooz shabani (everplays) <behrooz@rock.com>
2012-10-03 16:28:58 +0330

    don't need to do setupHtml when forwarding to 404

--- app/modules/Appliance/views/InfoErrorView.class.php
+++ app/modules/Appliance/views/InfoErrorView.class.php
@@ -42,8 +42,6 @@ class Appliance_InfoErrorView extends MarketApplianceBaseView
      */
     public function executeHtml(AgaviRequestDataHolder $rd)
     {
-        $this->setupHtml($rd);
-
         return $this->createForwardContainer(AgaviConfig::get('actions.error_404_module'), AgaviConfig::get('actions.error_404_action'));
     }
 }
