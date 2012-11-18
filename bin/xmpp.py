1b5137c4570ed83fdcd69d258b867138b476f2e0
behrooz shabani (everplays) <behrooz@rock.com>
2012-11-18 19:36:13 +0330

    fixed coding style issues of external daemon

--- bin/xmpp.py
+++ bin/xmpp.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
-# before running this script you need to have sleekxmpp and
-# redis installed, to do so run following:
-# pip install sleekxmpp
-# pip install redis
+"""
+before running this script you need to have sleekxmpp and
+redis installed, to do so run following:
+pip install sleekxmpp
+pip install redis
+"""
 
 # configuration
-marketJID="test-hypervisor@behrooz/market"
-marketPassword="test-hypervisor"
-peaceJID="behrooz@behrooz.mac/peace"
+marketJID = "test-hypervisor@behrooz/market"
+marketPassword = "test-hypervisor"
+peaceJID = "behrooz@behrooz.mac/peace"
 
 import xml.etree.ElementTree as ET
 from sleekxmpp import ClientXMPP
@@ -21,6 +23,7 @@ from time import sleep
 import logging
 from redis.exceptions import ConnectionError
 
+
 class Appliance(ElementBase):
     name = "appliance"
     namespace = ""
@@ -28,8 +31,13 @@ class Appliance(ElementBase):
     sub_interfaces = interfaces
 
     def setName(self, name):
+        """ set name of appliance.
+
+        @type name: string
+        @param name: the name of appliance
+
+        """
         self.xml.text = name
-        return name
 
     def getName(self):
         return self.xml.text
@@ -39,7 +47,13 @@ class Appliance(ElementBase):
         return self
 
     def setVersion(self, version):
-        return self._set_attr('version', str(version))
+        """ set version of appliance.
+
+        @type version: string
+        @param version: the version of appliance
+
+        """
+        self._set_attr('version', str(version))
 
     def getVersion(self):
         return self._get_attr('version')
@@ -48,6 +62,7 @@ class Appliance(ElementBase):
         self._del_attr('version')
         return self
 
+
 class Action(ElementBase):
     interfaces = set(("appliance"))
     sub_interfaces = interfaces
@@ -55,27 +70,53 @@ class Action(ElementBase):
     namespace = "market:xamin"
 
     def __init__(self, action, archipel, name, version):
+        """ the initializer of action iq request.
+
+        @type action: string
+        @param action: the action to be called. install or remove
+        @type archipel: string
+        @param archipel: the jid that archipel is connected to marketd with
+        @type name: string
+        @param name: the name of appliance to install
+        @type version: string
+        @param version: the version of appliance to install
+
+        """
         self.name = action
-        ElementBase.__init__(self);
-        if action=="install":
+        ElementBase.__init__(self)
+        if action == "install":
             self._set_attr('to', archipel)
-        elif action=="remove":
+        elif action == "remove":
             self._set_attr('from', archipel)
         appliance = Appliance(None, self)
         appliance['name'] = name
-        appliance['versiom'] = version
+        appliance['version'] = version
         self['appliance'] = appliance
 
+
 def connectToXmpp():
+    """ connect to xmpp.
+
+    @rtype ClientXMPP
+    @return the xmpp client
+
+    """
     c = ClientXMPP(marketJID, marketPassword)
     c.connect()
     return c
 
+
 def listenToChanges():
+    """ listen to actions that should get transfered. """
     while r.ping():
-        (key, j)= r.brpop('peace:daemon')
+        (key, j) = r.brpop('peace:daemon')
         action = json.loads(j)
-        action = Action(action['action'], action['jid'], action['name'], action['version'])
+        action = Action(
+            action['action'],
+            action['jid'],
+            action['name'],
+            action['version']
+        )
         try:
             iq = x.make_iq_set(sub=action, ito=peaceJID)
             iq.send()
@@ -88,15 +129,17 @@ def listenToChanges():
             print e
             r.lpush('peace:daemon', j)
 
+
 def connectToRedis():
-    host = "localhostx"
+    host = "localhost"
     port = 6379
-    database = ET.parse('../app/config/databases.xml').getroot().find('*').find('*').find('*')
+    xml = ET.parse('../app/config/databases.xml')
+    database = xml.getroot().find('*').find('*').find('*')
     for parameter in database.findall('*'):
-        if parameter.get('name')=="host":
+        if parameter.get('name') == "host":
             host = parameter.text
             continue
-        if parameter.get('name')=='port':
+        if parameter.get('name') == 'port':
             port = int(parameter.text)
             continue
     return redis.StrictRedis(host=host, port=port, db=0)
@@ -112,4 +155,3 @@ while True:
     except ConnectionError:
         print "connection to redis dropped, retrying in 10 seconds"
         sleep(10)
-
